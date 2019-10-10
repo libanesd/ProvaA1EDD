3ef7433106a67e84ffabfb6b839fa0d48482b8cb
Leonardo Zeaim <leonardozeaim@gmail.com>
2019-10-10 03:53:10 -0300

    First commit, upload directly from intelliJ IDEA

---
+++ src/com/unitins/FilaTest.java
@@ -0,0 +1,26 @@
+package com.unitins;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilaTest {
+
+    @org.junit.jupiter.api.Test
+    void estaVazia() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void estaCheia() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void adicionar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void remover() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mostrar() {
+    }
+}
---
+++ src/com/unitins/PilhaTest.java
@@ -0,0 +1,28 @@
+package com.unitins;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PilhaTest {
+
+    @Test
+    void pilhaVazia() {
+    }
+
+    @Test
+    void tamanho() {
+    }
+
+    @Test
+    void exibeUltimoValor() {
+    }
+
+    @Test
+    void desempilha() {
+    }
+
+    @Test
+    void empilhar() {
+    }
+}
